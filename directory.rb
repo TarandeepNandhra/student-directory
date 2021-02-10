18fff8b428d4ce866f69a8b30bca04896595da4c
EC2 Default User <tarandeep-nandhra@hotmail.co.uk>
2021-02-10 14:53:38 +0000

    Updated directory with students method

--- directory.rb
+++ directory.rb
@@ -12,6 +12,25 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+  
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -27,6 +46,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
