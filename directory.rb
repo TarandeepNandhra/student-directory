6d58b35eb90b0c9268881056bde08b9243e63e1c
EC2 Default User <tarandeep-nandhra@hotmail.co.uk>
2021-02-10 14:46:21 +0000

    Updated directory with methods

--- directory.rb
+++ directory.rb
@@ -12,8 +12,22 @@ students = [
   "Norman Bates"
 ]
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
-puts "The students of Villains Academy"
-puts "-------------"
-students.each { |name| puts name }
-print "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
+	
